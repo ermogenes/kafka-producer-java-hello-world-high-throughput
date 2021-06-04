048af699413115f311c9fa03cd1a3df29121e1b4
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-06-04 12:12:29 -0300

    Done

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# kafka-producer-java-hello-world
-A simple Java Kafka producer.
+# kafka-producer-java-hello-world-high-throughput
+A simple Java Kafka high throughput producer.
 
 ## Dependencies (`pom.xml`)
 
@@ -34,6 +34,10 @@ Properties properties = new Properties();
 properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
 properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
 
 KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 ProducerRecord<String, String> record = new ProducerRecord<String,String>(topic, message);
--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -24,7 +24,7 @@ public final class App {
         // create Producer properties
         // https://kafka.apache.org/documentation/#producerconfigs
 
-        String server = "172.25.145.150:9092";
+        String server = "172.20.229.109:9092";
         String topic = "first_topic";
         String message = "hello world!";
 
@@ -32,6 +32,11 @@ public final class App {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // high throughput config
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); // 32 KB
 
         // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
